f6ff563edac7522397d30fceb2f28b471faaf5c4
Juan Jose <jquinteros2022@gmail.com>
2025-09-20 08:40:36 -0600

    componentes parte 2

--- guia3componentes/Parte2/js/template.js
+++ guia3componentes/Parte2/js/template.js
@@ -3,19 +3,20 @@ const contenedor = document.getElementById("contenedor");
 const imagen = document.getElementById('miImagen');
 
 const productos = [
-    { titulo: "Laptop", desc: "16GB RAM",price:"100000",image: "./Img/Laptop.jpg" },
-    { titulo: "Mouse", desc: "Inalámbrico",price:"500000",image:"./Img/mouse.webp" },
-    { titulo: "Teclado", desc: "Inalámbrico",price:"100000",image: "./Img/teclado.avif"},
-    { titulo: "Celular", desc: "Amoled",price:"1200000",image: "./Img/iphone.jpg" },
-    { titulo: "Audifonos", desc: "Inalámbrico",price:"50000",image:"./Img/audifonos.jpg" },
+    { titulo: "Laptop", desc: "16GB RAM",price: 100000 ,image: "./Img/Laptop.jpg" },
+    { titulo: "Mouse", desc: "Inalámbrico",price: 500000 ,image:"./Img/mouse.webp" },
+    { titulo: "Teclado", desc: "Inalámbrico",price: 100000 ,image: "./Img/teclado.avif"},
+    { titulo: "Celular", desc: "Amoled",price: 1200000 ,image: "./Img/iphone.jpg" },
+    { titulo: "Audifonos", desc: "Inalámbrico",price: 50000,image:"./Img/audifonos.jpg" },
 ];
 
 productos.forEach(producto => {
     const clon = template.content.cloneNode(true);
     clon.querySelector(".title").textContent = producto.titulo;
     clon.querySelector(".desc").textContent = producto.desc;
-    clon.querySelector(".price").textContent = producto.price;
+    clon.querySelector(".price").textContent = producto.price.toLocaleString("Es-Co");
     clon.querySelector("img").src = producto.image;
+    clon.querySelector("img").title = producto.desc;
 
     contenedor.appendChild(clon);
 });
